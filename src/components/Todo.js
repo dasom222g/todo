4214e99969828d982888a66bfc866bcd8ec1c48b
dasom <dasom228@gmailcom>
2021-02-10 14:57:01 +0900

    FixTodoAndTodoList.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,32 +6,48 @@ import {RiCloseCircleLine} from 'react-icons/ri'
 import {TiEdit} from 'react-icons/ti'
 
 
-function Todo(todos, completeTodo) {
+function Todo({todos, completeTodo, removeTodo, updateTodo}) {
   const [edit, setEdit] = useState({
     id: null,
     value: ''
   })
 
-  const lists = []
-  todos.todos.map((todo, index) => {
-    lists.push(
-      <li key={index}>
-        <div
-          className={todo.isComplete ? 'todo-row complet' : 'todo-row'}
-        >
-          <div onClick={() => completeTodo(todo.id)}>{todo.text}</div>
-        </div>
-        <div className="icons">
-          <RiCloseCircleLine />
-          <TiEdit />
-        </div>
-      </li>
-    )
-  })
-  console.log('todos!!!!', todos.todos)
+  const submitUpdate = value => {
+    updateTodo(edit.id, value)
+    setEdit({
+      id: null,
+      value: ''
+    })
+  }
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />
+  }
+
   return (
     <ul className="todo-list">
-      {lists}
+      {todos.map((todo, index) => {
+        return <li key={index}>
+          <div
+            className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+            >
+            <div onClick={() => completeTodo(todo.id)}>{todo.text}</div>
+          </div>
+          <div className="icons">
+            <RiCloseCircleLine
+              onClick={() => removeTodo(todo.id)}
+              className="delete-icon"
+            />
+            <TiEdit
+              onClick={() => setEdit({
+                id: todo.id,
+                value: todo.text
+              })}
+              className="edit-icon"
+            />
+          </div>
+        </li>
+      })}
     </ul>
   )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -12,14 +12,28 @@ function TodoList() {
 
     const newTodo = [todo, ...todos] // todo: 현재 입력값, todos: 기존 입력값
     setTodos(newTodo)
-    console.log('addTodo실행')
-    console.log('todo', todo, 'todos', todos)
+    // console.log('addTodo실행')
+    // console.log('todo', todo, 'todos', todos)
   }
-  
+
+  const removeTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !== id)
+    setTodos(removeArr)
+  }
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return
+    }
+    console.log('update..')
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+  }
+
   const completeTodo = id => {
     let updatedTodos = todos.map(todo => {
       if (todo.id === id) {
         todo.isComplete = !todo.isComplete
+        console.log(todo.isComplete)
       }
       return todo
     })
@@ -30,7 +44,7 @@ function TodoList() {
     <div>
       <h1>What's the Plan for Today?</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
     </div>
   )
 }
